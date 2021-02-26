1976f645e5cf0eaabf6e2d4516f4db3f872c6836
Tien Nguyen Nhat <anonymous209184@gmail.com>
2021-02-26 21:01:13 +0700

    create MainTapUI

---
+++ Instagram_MVVM/Instagram_MVVM/Controller/MainTabController.swift
@@ -0,0 +1,19 @@
+//
+//  MainTabController.swift
+//  Instagram_MVVM
+//
+//  Created by NGUYENLONGTIEN on 2/26/21.
+//
+
+import UIKit
+
+class MainTabController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.backgroundColor = .red
+    }
+    
+
+}
