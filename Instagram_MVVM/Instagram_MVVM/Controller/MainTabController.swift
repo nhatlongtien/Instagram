666f5cd79f99beb437582ed6245ced700c6250e4
Tien Nguyen Nhat <anonymous209184@gmail.com>
2021-02-26 21:34:54 +0700

    Finish TapBar setup

--- Instagram_MVVM/Instagram_MVVM/Controller/MainTabController.swift
+++ Instagram_MVVM/Instagram_MVVM/Controller/MainTabController.swift
@@ -8,12 +8,35 @@
 import UIKit
 
 class MainTabController: UITabBarController {
-
+    //MARK: Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        self.view.backgroundColor = .red
+        configureViewController()
+        
     }
-    
+    //MARK: Helper Method
+    func configureViewController(){
+        self.view.backgroundColor = .red
+        let feed = templateNavigationController(unselectedImage: #imageLiteral(resourceName: "home_unselected"), selectedImage: #imageLiteral(resourceName: "home_selected"), rootViewController: FeedController())
+        
+        let search = templateNavigationController(unselectedImage: #imageLiteral(resourceName: "search_unselected"), selectedImage: #imageLiteral(resourceName: "search_selected"), rootViewController: SearchController())
+        
+        let imageSelector = templateNavigationController(unselectedImage: #imageLiteral(resourceName: "plus_unselected"), selectedImage: #imageLiteral(resourceName: "plus_unselected"), rootViewController: ImageSelectController())
+        
+        let notification = templateNavigationController(unselectedImage: #imageLiteral(resourceName: "like_unselected"), selectedImage: #imageLiteral(resourceName: "like_selected"), rootViewController: NotificationController())
+        
+        let profile = templateNavigationController(unselectedImage: #imageLiteral(resourceName: "profile_unselected"), selectedImage: #imageLiteral(resourceName: "profile_selected"), rootViewController: ProfileController())
+        
+        viewControllers = [feed, search, imageSelector, notification, profile]
+        
+        tabBar.tintColor = .black
+    }
+    func templateNavigationController(unselectedImage:UIImage, selectedImage:UIImage, rootViewController:UIViewController) -> UINavigationController{
+        let nav = UINavigationController(rootViewController: rootViewController)
+        nav.tabBarItem.image = unselectedImage
+        nav.tabBarItem.selectedImage = selectedImage
+        nav.navigationBar.tintColor = .black
+        return nav
+    }
 
 }
---
+++ Instagram_MVVM/Instagram_MVVM/Controller/SearchController.swift
@@ -0,0 +1,29 @@
+//
+//  SearchController.swift
+//  Instagram_MVVM
+//
+//  Created by NGUYENLONGTIEN on 2/26/21.
+//
+
+import UIKit
+
+class SearchController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.backgroundColor = .systemPink
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
